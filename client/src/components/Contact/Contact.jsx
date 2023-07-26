6a477c095335667ea2f40822e81f1cf0adb5e253
Ayush Desai <ayushdesai1000@gmail.com>
2023-07-26 14:27:28 +0530

    Contact us form done
    Contact us form done

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import './Contact.scss'
 const Contact = () => {
   return (
     <div className='Contact'>
-      Contact
+      <div className="wrapper">
+        <span>
+            Contact Us
+        </span>
+        <div className="email">
+            <input type='text' placeholder='example@planetcode.com'/>
+            <button>Submit</button>
+        </div>
+      </div>
     </div>
   )
 }
