7209e676bfede14d9cd8f25a05092c70cef35dc7
Ayush Desai <ayushdesai1000@gmail.com>
2023-08-03 16:45:42 +0530

    Search Button UI
    Search Button UI

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Search.scss';
+import search from '../../images/search.png';
 
 const Search = () => {
   return (
     <div className='Search'>
-      <input placeholder="Search"/>
+      <input placeholder="Search" />
+      <a className='btn'><img src={search} alt="Search" /></a>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
