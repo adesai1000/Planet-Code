a7c36653314ece7eb2ba14796c64f6b9cfe7a153
Ayush Desai <ayushdesai1000@gmail.com>
2023-08-08 14:55:47 +0530

    Bag is Empty
    Bag is Empty

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -3,41 +3,55 @@ import './Cart.scss'
 import trash20 from '../../images/trash20.png'
 import { useSelector, useDispatch } from 'react-redux'
 import { removeItem, resetCart } from '../../redux/cartReducer'
+
 const Cart = () => {
     const products = useSelector(state => state.cart.products)
     const dispatch = useDispatch()
+
     const totalPrice = () => {
         let total = 0;
         products.forEach((item) => (total += item.price))
         return total.toLocaleString()
     }
+
     return (
         <div className='cart'>
             <h1>Items in your bag</h1>
-            {products?.map(item => (
-                <div className='item' key={item.id}>
-                    <img src={process.env.REACT_APP_UPLOAD_URL + item.image} alt={item.title} />
-                    <div className="details">
-                        <h1>{item.title}</h1>
-                        <p>{item.description?.substring(0, 30)}...</p>
-                        <div className="price">
-                            ${item.price.toLocaleString()}
+            {products.length === 0 ? (
+                <div className='empty-cart'><h3>Your Bag is Empty< br/>< br/>Shop Now!</h3></div>
+                
+            ) : (
+                products.map(item => (
+                    <div className='item' key={item.id}>
+                        <img src={process.env.REACT_APP_UPLOAD_URL + item.image} alt={item.title} />
+                        <div className="details">
+                            <h1>{item.title}</h1>
+                            <p>{item.description?.substring(0, 30)}...</p>
+                            <div className="price">
+                                ${item.price.toLocaleString()}
+                            </div>
+                        </div>
+                        <div className="trash" onClick={() => {
+                            console.log('Removing item:', item.id);
+                            dispatch(removeItem(item.id))
+                        }}>
+                            <img src={trash20} alt="Delete" />
                         </div>
                     </div>
-                    <div className="trash"  onClick={() =>{
-                         console.log('Removing item:', item.id);
-                         dispatch(removeItem(item.id))}
-                    }>
-                        <img src={trash20} />
-                    </div>
+                ))
+            )}
+            {products.length > 0 && (
+                <div className="total">
+                    <span>Total</span>
+                    <span className='TotalP'>${totalPrice()}</span>
                 </div>
-            ))}
-            <div className="total">
-                <span>Total</span>
-                <span className='TotalP'>${totalPrice()}</span>
-            </div>
-            <button>Check Out</button>
-            <span className='reset' onClick={() => dispatch(resetCart())}>Clear Bag</span>
+            )}
+            {products.length > 0 && (
+                <button>Check Out</button>
+            )}
+            {products.length > 0 && (
+                <span className='reset' onClick={() => dispatch(resetCart())}>Clear Bag</span>
+            )}
         </div>
     )
 }
